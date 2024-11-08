61a927de02918e72d5af1211c2bbf1b0946fef26
Diaa Mahfouz <mahfouzdiaa5@gmail.com>
2024-11-08 19:48:40 +0200

    add  fetch notes method

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants/constants.dart';
 import 'package:notes_app/models/notes_model.dart';
 
 part 'notes_state.dart';
 
-class NotesCubitCubit extends Cubit<NotesCubitState> {
-  NotesCubitCubit() : super(NotesCubitInitial());
+class NotesCubit extends Cubit<NotesCubitState> {
+  NotesCubit() : super(NotesCubitInitial());
+
+  fetchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NotesModel>(KnotesBox);
+
+      List<NotesModel> notesList = notesBox.values.toList();
+      emit(NotesSuccess(notesList));
+    } catch (e) {
+      emit(Notesfailure(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
@@ -8,25 +10,28 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.greenAccent,
-          shape: const CircleBorder(),
-          onPressed: () {
-            showModalBottomSheet(
-              isScrollControlled: true,
-              context: context,
-              builder: (_) => AddNoteBottomSheet(),
-            );
-          },
-          child: Icon(
-            Icons.add,
-            size: 25,
-            color: Colors.black,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: SafeArea(
+        child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.greenAccent,
+            shape: const CircleBorder(),
+            onPressed: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (_) => AddNoteBottomSheet(),
+              );
+            },
+            child: Icon(
+              Icons.add,
+              size: 25,
+              color: Colors.black,
+            ),
           ),
+          body: NotesViewBody(),
         ),
-        body: NotesViewBody(),
       ),
     );
   }
